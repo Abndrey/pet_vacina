7201d426133013d09e5fbabd9f30ade947b37074
Abndrey <160252981+Abndrey@users.noreply.github.com>
2025-03-12 19:53:44 -0300

    Add files via upload

---
+++ pet_vacina.js
@@ -0,0 +1,13 @@
+var nome = prompt("Insira seu nome ");
+var email = prompt("Insira seu email");
+var cartao = prompt("insira seu CPF");
+var vacina_cartao = prompt("Insira a data de nascimento");
+var vacina_seguranca = prompt("Insira o CNPJ");
+var senha = prompt("Insira a senha");
+
+console.log(nome);
+console.log(email);
+console.log(cartao);
+console.log(vacina_seguranca);
+console.log(vacina_cartao);
+console.log(senha);
